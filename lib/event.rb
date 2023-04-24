b4e5a9988c1da0690cc381bd2d234a6ece0db4f0
Camille Lenormand <camille.lenormand02@gmail.com>
2023-04-24 14:48:43 +0200

    add end date method

--- lib/event.rb
+++ lib/event.rb
@@ -1,4 +1,5 @@
 require 'time'
+require 'pry'
 
 class Event
   attr_accessor :start_date, :attendees, :duration, :title
@@ -14,10 +15,26 @@ class Event
     Event.new(start_date: date, duration: duration, attendees: attendees, title: title)
   end
 
+  def postpone_event
+    return @start_date += 86400
+  end
+
+ # binding.pry
+
+  def get_end_date
+    end_date = @start_date + (@duration * 60)
+    puts "End date ---- #{end_date} ----"
+  end
+
+
   def to_s
-    "Date : #{@start_date}\nDuration : #{@duration}\nTitle : #{@title}\nAttendees : #{@attendees.join(", ")}"
+    "Date : ----#{@start_date}----\nDuration : #{@duration} minutes\nTitle : #{@title}\nAttendees : #{@attendees.join(", ")}"
   end
 end
 
 event = Event.create_event(date: "2019-01-13 09:00", duration: 10, title: "standup quotidien", attendees: ["truc@machin.com", "bidule@chose.fr"])
+puts event
+event.postpone_event
+puts event
+event.get_end_date
 puts event
