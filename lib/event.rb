02c2906f70cb6b6bae9bde69057c5db10ae2fb7d
Camille Lenormand <camille.lenormand02@gmail.com>
2023-04-24 15:09:10 +0200

    past, future and soon methods added

--- lib/event.rb
+++ lib/event.rb
@@ -26,6 +26,17 @@ class Event
     puts "End date ---- #{end_date} ----"
   end
 
+  def is_past?
+    @start_date < Time.now ? true : false
+  end
+
+  def is_future?
+    @start_date > Time.now ? true : false
+  end
+
+  def is_soon?
+    Time.now() - @start_date < 30
+  end
 
   def to_s
     "Date : ----#{@start_date}----\nDuration : #{@duration} minutes\nTitle : #{@title}\nAttendees : #{@attendees.join(", ")}"
@@ -33,8 +44,9 @@ class Event
 end
 
 event = Event.create_event(date: "2019-01-13 09:00", duration: 10, title: "standup quotidien", attendees: ["truc@machin.com", "bidule@chose.fr"])
-puts event
-event.postpone_event
-puts event
+#event.postpone_event
 event.get_end_date
+puts "Event is due." if event.is_past?
+puts "Event starting later." if event.is_future?
+puts "Event starting very soon." if event.is_soon?
 puts event
