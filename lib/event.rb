a8ac9d467330039fa9c6e36be2d3a22dc7cfd0f6
Camille Lenormand <camille.lenormand02@gmail.com>
2023-04-24 15:12:46 +0200

    add comments

--- lib/event.rb
+++ lib/event.rb
@@ -5,48 +5,46 @@ class Event
   attr_accessor :start_date, :attendees, :duration, :title
 
   def initialize(start_date:, duration:, attendees:, title:)
-    @start_date = Time.parse(start_date)
-    @duration = duration
-    @title = title
-    @attendees = attendees
+    @start_date = Time.parse(start_date) #parse the start date to Time object
+    @duration = duration #duration of the event
+    @title = title #title of the event
+    @attendees = attendees #attendees of the event
   end
 
   def self.create_event(date:, duration:, attendees:, title:)
-    Event.new(start_date: date, duration: duration, attendees: attendees, title: title)
+    Event.new(start_date: date, duration: duration, attendees: attendees, title: title) #create a new event object
   end
 
   def postpone_event
-    return @start_date += 86400
+    return @start_date += 86400 #postpone the event by one day
   end
 
- # binding.pry
-
   def get_end_date
-    end_date = @start_date + (@duration * 60)
-    puts "End date ---- #{end_date} ----"
+    end_date = @start_date + (@duration * 60) #calculate the end date of the event
+    puts "End date ---- #{end_date} ----" #print the end date of the event
   end
 
   def is_past?
-    @start_date < Time.now 
+    @start_date < Time.now #check if the event is in the past
   end
 
   def is_future?
-    @start_date > Time.now
+    @start_date > Time.now #check if the event is in the future
   end
 
   def is_soon?
-    Time.now() - @start_date < 30
+    Time.now() - @start_date < 30 #check if the event is starting soon
   end
 
   def to_s
-    "Date : ----#{@start_date}----\nDuration : #{@duration} minutes\nTitle : #{@title}\nAttendees : #{@attendees.join(", ")}"
+    "Date : ----#{@start_date}----\nDuration : #{@duration} minutes\nTitle : #{@title}\nAttendees : #{@attendees.join(", ")}" #return the event details as a string
   end
 end
 
-event = Event.create_event(date: "2019-01-13 09:00", duration: 10, title: "standup quotidien", attendees: ["truc@machin.com", "bidule@chose.fr"])
-event.postpone_event
-event.get_end_date
-puts "Event is due." if event.is_past?
-puts "Event starting later." if event.is_future?
-puts "Event starting very soon." if event.is_soon?
-puts event
+event = Event.create_event(date: "2019-01-13 09:00", duration: 10, title: "standup quotidien", attendees: ["truc@machin.com", "bidule@chose.fr"]) #create a new event object
+event.postpone_event #postpone the event by one day
+event.get_end_date #print the end date of the event
+puts "Event is due." if event.is_past? #check if the event is in the past and print a message
+puts "Event starting later." if event.is_future? #check if the event is in the future and print a message
+puts "Event starting very soon." if event.is_soon? #check if the event is starting soon and print a message
+puts event #print the event details
